3dad23b42bd1e6cd73cae0a026e9f495ffdb3e29
Iain F <iain@feedthemouse.com>
2023-12-30 11:48:57 +0000

    Rename integration in text

--- custom_components/predbat_ha/const.py
+++ custom_components/predbat_ha/const.py
@@ -1,9 +1,9 @@
-"""Constants for integration_blueprint."""
+"""Constants for predbat_ha."""
 from logging import Logger, getLogger
 
 LOGGER: Logger = getLogger(__package__)
 
 NAME = "Integration blueprint"
-DOMAIN = "integration_blueprint"
+DOMAIN = "predbat_ha"
 VERSION = "0.0.0"
 ATTRIBUTION = "Data provided by http://jsonplaceholder.typicode.com/"
--- custom_components/predbat_ha/switch.py
+++ custom_components/predbat_ha/switch.py
@@ -1,4 +1,4 @@
-"""Switch platform for integration_blueprint."""
+"""Switch platform for predbat_ha."""
 from __future__ import annotations
 
 from homeassistant.components.switch import SwitchEntity, SwitchEntityDescription
@@ -9,7 +9,7 @@ from .entity import IntegrationBlueprintEntity
 
 ENTITY_DESCRIPTIONS = (
     SwitchEntityDescription(
-        key="integration_blueprint",
+        key="predbat_ha",
         name="Integration Switch",
         icon="mdi:format-quote-close",
     ),
@@ -29,7 +29,7 @@ async def async_setup_entry(hass, entry, async_add_devices):
 
 
 class IntegrationBlueprintSwitch(IntegrationBlueprintEntity, SwitchEntity):
-    """integration_blueprint switch class."""
+    """predbat_ha switch class."""
 
     def __init__(
         self,
